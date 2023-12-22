35aeec26507c8d01708f747a3835e196b0a63da7
Nishant Siddh <94243692+nishant-siddh@users.noreply.github.com>
2023-12-22 12:54:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-import { Outlet } from 'react-router-dom'
-import './App.css'
-import Header from './Components/Header'
-import Footer from './Components/Footer'
+import { Outlet } from "react-router-dom";
+import "./App.css";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const lazyDropAnimations = document.querySelectorAll(
+      ".lazy-drop-animation"
+    );
+
+    lazyDropAnimations.forEach((element, index) => {
+      element.style.animationDelay = index * 0.5 + "s";
+    });
+  }, []);
 
   return (
-    <main className='max-h-screen flex flex-col'>
+    <main className="max-h-screen flex flex-col">
       <Header />
       <Outlet />
       <Footer />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
