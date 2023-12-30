dff2a899f8bc8e692312406cc5eab14d59885cc9
Nishant Siddh <94243692+nishant-siddh@users.noreply.github.com>
2023-12-30 23:22:54 +0530

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,5 @@
 import logo from "../images/logo.png";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { useEffect, useState } from "react";
 import { IoMdArrowDropdown } from "react-icons/io";
@@ -9,6 +9,7 @@ import { RxCross1 } from "react-icons/rx";
 const Header = () => {
   const [isServicesOpen, setIsServicesOpen] = useState(false);
   const [isNavOpen, setIsNavOpen] = useState(false);
+  const location = useLocation();
   const services = [
     {
       to: "rajasthani-mehndi",
@@ -44,9 +45,12 @@ const Header = () => {
     }
   }, [isServicesOpen]);
 
-  // window.addEventListener('click',()=>{
-  //   setIsServicesOpen(true);
-  // });
+  useEffect(() => {
+    const aboutSection = document.getElementById("aboutSection");
+    if (location.hash === "#aboutSection") {
+      aboutSection.scrollIntoView({ behavior: "smooth" });
+    }
+  }, []);
 
   return (
     <nav
@@ -55,25 +59,56 @@ const Header = () => {
       <div
         className={`container mx-auto md:px-10 flex justify-between items-center transition-all duration-300`}
       >
-        <img src={logo} alt="Logo" className={`lazy-drop-animation w-24 h-20 sm:w-28 sm:h-24`} />
-        <div className={`w-full sm:w-fit m-0 absolute top-[5.3rem] left-0 sm:relative sm:top-0 bg-[#4E6659] ${isNavOpen ? 'py-5' : 'py-0'} transition-all duration-300`}>
-          <ul className={`list ${isNavOpen ? "flex flex-col gap-5 mx-auto" : 'hidden'} sm:flex-row justify-between items-center gap-10 sm:flex text-[#FFFFF0]/90 hover: text-[#FFFFF0]`}>
+        <img
+          src={logo}
+          alt="Logo"
+          className={`lazy-drop-animation w-24 h-20 sm:w-28 sm:h-24`}
+        />
+        <div
+          className={`w-full sm:w-fit m-0 absolute top-[5.3rem] left-0 sm:relative sm:top-0 bg-[#4E6659] ${
+            isNavOpen ? "py-5" : "py-0"
+          } transition-all duration-300`}
+        >
+          <ul
+            className={`list ${
+              isNavOpen ? "flex flex-col gap-5 mx-auto" : "hidden"
+            } sm:flex-row justify-between items-center gap-10 sm:flex text-[#FFFFF0]/90 hover: text-[#FFFFF0]`}
+          >
             <li>
               <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <a href="/#aboutSection" className="cursor-pointer">
+                About Us
+              </a>
             </li>
             <li>
               <NavLink to="/gallery">Gallery</NavLink>
             </li>
             <li>
-              <div className="flex items-center cursor-pointer" onClick={() => setIsServicesOpen(prev => !prev)}>
+              <div
+                className="flex items-center cursor-pointer"
+                onClick={() => setIsServicesOpen((prev) => !prev)}
+              >
                 <NavLink to="#">Services</NavLink>
-                <span className="text-lg">{isServicesOpen ? <IoMdArrowDropup /> : <IoMdArrowDropdown />}</span>
+                <span className="text-lg">
+                  {isServicesOpen ? <IoMdArrowDropup /> : <IoMdArrowDropdown />}
+                </span>
               </div>
-              <div className={`absolute bg-[#FFFFF0] z-50 top-10 sm:top-14 left-0 w-44 text-[#4E6659] flex-col ${isServicesOpen ? 'flex' : 'hidden'}`}>
+              <div
+                className={`absolute bg-[#FFFFF0] z-50 top-10 sm:top-14 left-0 w-44 text-[#4E6659] flex-col ${
+                  isServicesOpen ? "flex" : "hidden"
+                }`}
+              >
                 {services.map((service) => (
                   <>
                     <span className="py-2 px-3 hover:bg-[#e7a316] hover:text-[#FFFFF0]">
-                      <NavLink to={`/services/${service.to}`} onClick={() => { setIsServicesOpen(false), setIsNavOpen(false) }}>
+                      <NavLink
+                        to={`/services/${service.to}`}
+                        onClick={() => {
+                          setIsServicesOpen(false), setIsNavOpen(false);
+                        }}
+                      >
                         {service.name}
                       </NavLink>
                     </span>
@@ -87,8 +122,15 @@ const Header = () => {
           </ul>
         </div>
 
-        <div className="flex sm:hidden" onClick={() => setIsNavOpen(prev => !prev)}>
-          {!isNavOpen ? <GiHamburgerMenu className="text-[#FFFFF0] text-lg" /> : <RxCross1 className="text-[#FFFFF0] text-lg" />}
+        <div
+          className="flex sm:hidden"
+          onClick={() => setIsNavOpen((prev) => !prev)}
+        >
+          {!isNavOpen ? (
+            <GiHamburgerMenu className="text-[#FFFFF0] text-lg" />
+          ) : (
+            <RxCross1 className="text-[#FFFFF0] text-lg" />
+          )}
         </div>
       </div>
     </nav>
