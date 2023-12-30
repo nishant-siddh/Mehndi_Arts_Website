211dc2e24fd362035b1b10c7e008de6b597922f1
Nishant Siddh <94243692+nishant-siddh@users.noreply.github.com>
2023-12-30 23:26:26 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -2,7 +2,7 @@ import aboutImg from "../images/extra/aboutImage.png";
 
 const About = () => {
   return (
-    <div className="mt-20 bg-[#ffffeb] py-20" id="aboutSection">
+    <div className="mt-[6.5rem] bg-[#ffffeb] py-[6.5rem]" id="aboutSection">
       <div className="container mx-auto flex flex-wrap gap-7 items-center justify-center">
         <div className="md:w-[40%]">
           <img
